20522fdbcb9fb6d41efbb057e02743cc2e363a8c
Arshya Soleimani <arrshya@gmail.com>
2023-08-27 16:33:14 -0600

    add onto tweet_detail_view to send back JSON

--- tweets/views.py
+++ tweets/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, JsonResponse
 from .models import Tweet
 
 
@@ -8,5 +8,17 @@ def home_view(request, *args, **kwargs):
     return HttpResponse("<h1>Hello World</h1>")
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
-    obj = Tweet.objects.get(id=tweet_id)
-    return HttpResponse(f"<h1> Hello {tweet_id} - {obj.content}</h1>")
+    data = {
+       "id": tweet_id,
+    }
+    
+    status = 200
+    
+    try:
+      obj = Tweet.objects.get(id=tweet_id)
+      data['content'] = obj.content
+    except:
+       data['message'] = "Not Found"
+       status = 404
+      
+    return JsonResponse(data, status=status)
