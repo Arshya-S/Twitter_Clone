2c538fd9df7bf035d3cfc18ba91b167a577384c1
Arshya Soleimani <arrshya@gmail.com>
2023-08-27 16:18:51 -0600

    create basic views using the Tweet model

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Tweet
+
 
 # Create your views here.
+def home_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Hello World</h1>")
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    obj = Tweet.objects.get(id=tweet_id)
+    return HttpResponse(f"<h1> Hello {tweet_id} - {obj.content}</h1>")
