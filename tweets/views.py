3845c75608b77c294b8eccbe4d63060013583c42
Arshya Soleimani <arrshya@gmail.com>
2023-09-06 14:23:09 -0600

    format tweets using inner HTML and add Meta class to model for ordering by id decending

--- tweets/views.py
+++ tweets/views.py
@@ -39,7 +39,7 @@ def tweet_create(request, *args, **kwargs):
       # do other form related logic
       obj.save()
       if request.is_ajax():
-         return JsonResponse({}, status=201) # 201 == created items
+         return JsonResponse(obj.serialize(), status=201) # 201 == created items
       if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
          return redirect(next_url)
       form = TweetForm()
