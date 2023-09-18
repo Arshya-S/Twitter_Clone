29e2103c2e2a13a163c77f83fd153dc12e003720
Arshya Soleimani <arrshya@gmail.com>
2023-09-18 13:21:02 -0600

    fixed retweet bug

--- tweets/serializers.py
+++ tweets/serializers.py
@@ -46,13 +46,16 @@ class TweetSerializer(serializers.ModelSerializer):
 
   class Meta:
     model = Tweet
-    fields = ['id', 'content', 'likes']
+    fields = ['id', 'content', 'likes', 'is_retweet']
   
   def get_likes(self, obj):
     return obj.likes.count()
   
   def get_content(self, obj):
-    return obj.content
+    content = obj.content
+    if obj.is_retweet:
+      content = obj.parent.content
+    return content
 
 
 
