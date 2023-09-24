8108bc27c859876463e4e6af4df4f990d3cd6460
Arshya Soleimani <arrshya@gmail.com>
2023-09-24 15:20:02 -0600

    implement liking feature on client side

--- frontend/src/components/Tweet.tsx
+++ frontend/src/components/Tweet.tsx
@@ -1,17 +1,24 @@
+import { useState } from "react"
 import { TweetType } from "./TweetsList"
 
 
 const Tweet: React.FC<{ tweet: TweetType}> = ({tweet}) => {
+
+  const [liked, setliked] = useState<boolean>(false)
+  // const likedButtonColor: string[] = ['btn-outline-primary', 'btn-primary']
+  const colorFilled: string = 'btn-primary'
+  const colorEmpty: string = 'btn-outline-primary'
+  const color: string = liked ? colorFilled : colorEmpty
   return(
     <div className="col-7 mx-auto p-3 mt-4 border border-2">
       <p className='ms-2 fw-normal'>{tweet.content}</p>
       <div className='row align-items-center'>
-        <button className="ms-3 col-2 btn btn-outline-primary btn-sm">Like {tweet.likes}</button> 
+        <button className={`ms-3 col-2 btn ${color} btn-sm`} onClick={() => {
+          setliked(!liked)
+        }}>Like {tweet.likes}</button> 
         <button className="ms-3 col-2 btn btn-outline-secondary btn-sm">Retweet</button> 
       </div>
-    </div>
-
-
+    </div> 
   )
 }
 
