dc8f49a579deffd75c9632851f3b3c40b849513a
Arshya Soleimani <arrshya@gmail.com>
2023-09-24 16:10:43 -0600

    tweet form functions as expected on submission

--- frontend/src/components/CreateTweet.tsx
+++ frontend/src/components/CreateTweet.tsx
@@ -1,16 +1,32 @@
+import { useState } from "react"
+
 const CreateTweet = () => {
-  return(
-    <form className="col-7 mx-auto d-flex flex-column align-items-end mt-5" action="" onSubmit={(event) => {
-      event.preventDefault()
-      console.log(event)
-    }}>
-        <textarea name='tweet' className="form-control"></textarea>
-        <button type="submit" className="btn btn-primary btn-sm mt-1 mb-3">Tweet</button>
+  const [text, setText] = useState<string>('')
+
+  const handleTextareaChange = (event:  React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(event.target.value)
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault()
+    console.log(text)
+    setText('')
+  };
+
+  return (
+    <form
+      className="col-7 mx-auto d-flex flex-column align-items-end mt-5"
+      onSubmit={handleSubmit}
+    >
+      <textarea
+        value={text}
+        onChange={handleTextareaChange}
+        className="form-control"
+        placeholder="Enter your tweet here"
+      ></textarea>
+      <button type="submit" className="btn btn-primary btn-sm mt-1 mb-3">Tweet</button>
     </form>
   )
-
-
-
 }
 
 
