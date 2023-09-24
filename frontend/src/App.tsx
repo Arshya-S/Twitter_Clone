b9a47b2970ab343d0490895fc7339173a03d718d
Arshya Soleimani <arrshya@gmail.com>
2023-09-24 14:20:13 -0600

    Add Tweet List component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,52 +1,15 @@
-import { useState, useEffect } from 'react'
 import './App.css'
 import './index.css'
 
-import Tweet from './components/Tweet'
+import TweetList from './components/TweetsList'
 
-export interface TweetType {
-  id: number,
-  content: string,
-  likes: number,
-  is_retweet: boolean,
-  parent: object
-}
+
+
+
 
 function App() {
-  const [tweets, setTweets] = useState<TweetType[]>([]);
-
-  
-  useEffect(() => {
-    
-    const getTweets = async () => {
-      try {
-        const response = await fetch('http://127.0.0.1:8000/api/tweets', {
-          method: 'GET'
-        })
-
-        if(!response.ok) {
-          throw new Error('Response not ok')
-        }
-
-        const data: TweetType[] = await response.json();
-        setTweets(data)
-
-      } catch (err) {
-        console.log('Fetch tweets error: ', err)
-      }
-    }
-
-    getTweets();
-  }, [])
-
-
-
   return (
-    <div className='container'>
-      {tweets.map((tweet, index) => {
-        return <Tweet key={index} tweet={tweet}/>
-      })}
-    </div>
+    <TweetList />
   )
 }
 
---
+++ frontend/src/components/TweetsList.tsx
@@ -0,0 +1,49 @@
+import Tweet from "./Tweet"
+import { useEffect, useState } from "react"
+
+export interface TweetType {
+  id: number,
+  content: string,
+  likes: number,
+  is_retweet: boolean,
+  parent: object
+}
+
+const TweetList = () => {
+
+  const [tweets, setTweets] = useState<TweetType[]>([]);
+
+  useEffect(() => {
+    
+    const getTweets = async () => {
+      try {
+        const response = await fetch('http://127.0.0.1:8000/api/tweets', {
+          method: 'GET'
+        })
+
+        if(!response.ok) {
+          throw new Error('Response not ok')
+        }
+
+        const data: TweetType[] = await response.json();
+        setTweets(data)
+
+      } catch (err) {
+        console.log('Fetch tweets error: ', err)
+      }
+    }
+
+    getTweets();
+  }, [])
+
+  return (
+    <div className='container'>
+      {tweets.map((tweet, index) => {
+        return <Tweet key={index} tweet={tweet}/>
+      })}
+    </div>
+  )
+
+}
+
+export default TweetList
